a9c79cffc1f2612f68a239146eaa32ef314d9959
Artur Halili <haliliartur894@gmail.com>
2025-10-26 13:00:41 +0100

    m

--- front-page.php
+++ front-page.php
@@ -4,9 +4,24 @@
     <h2>Featured Phones</h2>
     <div class="products">
         <div class="product">
+            <img src="https://www.apple.com/newsroom/images/2024/09/apple-debuts-iphone-16-pro-and-iphone-16-pro-max/article/Apple-iPhone-16-Pro-hero-240909_inline.jpg.large_2x.jpg" alt="iPhone">
+            <h3>iPhone 16 Pro/Pro Max</h3>
+            <p>Beauty and Performance to Match 900$/950$</p>
+        </div>
+                <div class="product">
+            <img src="https://www.apple.com/newsroom/images/2024/09/apple-debuts-iphone-16-pro-and-iphone-16-pro-max/article/Apple-iPhone-16-Pro-hero-240909_inline.jpg.large_2x.jpg" alt="iPhone">
+            <h3>iPhone 16 Pro/Pro Max</h3>
+            <p>Beauty and Performance to Match 900$/950$</p>
+        </div>
+        <div class="product">
+            <img src="https://www.apple.com/newsroom/images/2023/09/apple-unveils-iphone-15-pro-and-iphone-15-pro-max/article/Apple-iPhone-15-Pro-lineup-hero-230912_Full-Bleed-Image.jpg.xlarge_2x.jpg" alt="iPhone">
+            <h3>iPhone 15 Pro/Pro Max</h3>
+            <p>Enduring and Stylish Titanium with High Performance. 800$/850$</p>
+        </div>
+        <div class="product">
             <img src="https://upload.wikimedia.org/wikipedia/commons/f/fe/IPhone_14_Pro_images_d%27illustration.jpg" alt="iPhone">
-            <h3>iPhone 14 Pro</h3>
-            <p>Powerful. Elegant. Fast. Starting at $999</p>
+            <h3>iPhone 14 Pro/Pro Max</h3>
+            <p>Powerful. Elegant. Fast. $650/700$</p>
         </div>
         <div class="product">
             <img src="https://images.unsplash.com/photo-1603791440384-56cd371ee9a7" alt="Samsung">
